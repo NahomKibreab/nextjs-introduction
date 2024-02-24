fc048814220f7b173fade756205a63ec9b933019
NahomKibreab <nknahom@gmail.com>
2024-02-24 11:54:02 -0700

    Update link on product page

--- app/products/[product]/page.js
+++ app/products/[product]/page.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Page({ params }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Products: {params.product}</h1>
+      <Link href="/products">Back to products</Link>
     </main>
   );
 }
